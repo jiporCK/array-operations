bb862c72134988ab5d38f9f037c27a7b3bfcc582
jiporCK <srengchipor99@gmail.com>
2025-05-29 16:07:06 +0700

    insert and delete array

--- ArrayOperation.java
+++ ArrayOperation.java
@@ -5,10 +5,7 @@ public class ArrayOperation {
     public static void main(String[] args) {
 
         // Basic Array Operations
-        int[] arr = {10, 9, 5, 4, 2, 18};
-
-        int pos = 3;
-        arr[pos] = 7;
+        int[] arr = {10, 9, 2, 5, 4, 2, 18};
         for (int a: arr) {
             System.out.print(a + " ");
         }
@@ -27,10 +24,35 @@ public class ArrayOperation {
                 j++;
             }
         }
-        arr = newArr;
+        arr = newArr; // assign new array to the original array
         for (int j : arr) {
             System.out.print(j + " ");
         }
+        System.out.println();
+
+        //Delete element from array
+        int elementToDelete = 2;
+        // to count element that is not equal to 2
+        int count = 0;
+        for (int num : arr) {
+            if (num != elementToDelete) {
+                count++;
+            }
+        }
+        System.out.println(count);
+        int[] arrAfterDelete = new int[count];
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != elementToDelete) {
+                arrAfterDelete[j] = arr[i];
+                j++;
+            }
+        }
+        arr = arrAfterDelete;
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
 
     }
 
